8c69dd411344eeafe8bc7638a05f3d7faa1fd5b9
Devopswithaws58 <devopswithaws58@gmail.com>
2022-12-27 18:52:34 +0530

    first commit

---
+++ src/main/java/com/example/springbootmavenrestapi/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.springbootmavenrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	public String welcome(){
+	return "welcome to spring boot";
+}}
+
---
+++ src/test/java/com/example/springbootmavenrestapi/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmavenrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
