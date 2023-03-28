efccda7eab7f7f54e9d3cc45cca4c6c547c9d37d
Devopswithaws58 <121493588+Devopswithaws58@users.noreply.github.com>
2023-03-28 19:00:14 +0530

    Update DemoApplication.java

--- src/main/java/com/example/springbootmavenrestapi/DemoApplication.java
+++ src/main/java/com/example/springbootmavenrestapi/DemoApplication.java
@@ -4,13 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	
-	public String welcome(){
-	return "welcome to spring boot";
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "welcome to spring boot rest api";
+	}
 }}
 
